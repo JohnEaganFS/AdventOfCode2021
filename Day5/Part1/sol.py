9609e02a4d601a75d5c2c215d71df0e3759c5702
John Eagan <johnjeagan@mail.fresnostate.edu>
2021-12-10 13:58:38 -0800

    day 5

---
+++ Day5/Part1/sol.py
@@ -0,0 +1,54 @@
+import os
+
+if __name__ == "__main__":
+    filename = "Day5/Part1/puzzle.txt"
+    with open(filename, "r") as file:
+        lines = file.readlines()
+    lines = [line.strip("\n") for line in lines]
+
+    firstPoints = []
+    secondPoints = []
+    for line in lines:
+        splitText = line.split()
+        firstText = splitText[0]
+        firstSplit = firstText.split(',')
+        firstPoints.append((int(firstSplit[0]), int(firstSplit[1])))
+
+        secondText = splitText[2]
+        secondSplit = secondText.split(',')
+        secondPoints.append((int(secondSplit[0]), int(secondSplit[1])))
+    #print(firstPoints)
+    #print(secondPoints)
+
+    linePoints = list(zip(firstPoints, secondPoints))
+    #print(linePoints)
+
+    board = dict()
+
+    for points in linePoints:
+        p1 = points[0]
+        p2 = points[1]
+        
+        x1, y1 = p1
+        x2, y2 = p2
+        
+        if x1 == x2:
+            yMin = min([y1, y2])
+            yMax = max([y1, y2])
+            for i in range(yMin, yMax + 1):
+                if (x1, i) in board:
+                    board[(x1, i)] += 1
+                else:
+                    board[(x1, i)] = 1
+        elif y1 == y2: # y1 == y2
+            xMin = min([x1, x2])
+            xMax = max([x1, x2])
+            for i in range(xMin, xMax + 1):
+                if (i, y1) in board:
+                    board[(i, y1)] += 1
+                else:
+                    board[(i, y1)] = 1
+    
+    print(board)
+    ans = sum([1 for val in board.values() if val >= 2])
+    print(ans) # 8111
